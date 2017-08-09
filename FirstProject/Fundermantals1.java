b4b4f9852e799077ae86225f84f24089cff8208d
cmis <cmis@cmis-hs03.(none)>
2017-08-09 14:35:43 +0700

    started on primitive type program

---
+++ FirstProject/Fundermantals1.java
@@ -0,0 +1,43 @@
+
+/**
+ * Write a description of class Fundermantals1 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Fundermantals1
+{
+   public static void main (String[] args) {
+   
+       String helloWorld = "Hello World";
+       
+       System.out.println(helloWorld+"!");
+    
+   }
+   
+   public static void dataTypes () {
+       
+       // A byte is a type that has a limited numeric range which allows it to be quickly applied in computations. Cannot be decimal.. 
+       byte byteType = -5;
+       
+       // A short has the same purpose as a byte  (save memory) but has a larger range. Cannot be decimal.
+       short shortType = 40;
+       
+       // An int is double the size of a short but has an even larger range. Cannot be decimal.
+       int intType = 4;
+       
+       // A long is larger than an int but uses a lot more memory.
+       long longType = 987654321;
+       
+       float floatType = 5.61f;
+       
+       double doubleType = 4.505;
+       
+       boolean boolType = true;
+       
+       char charType = 'a';
+       
+   }
+   
+   
+}
