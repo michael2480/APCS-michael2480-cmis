90998ee5f32b0817bafad99d67d511e8308e7777
cmis <cmis@cmis-hs03.(none)>
2017-08-30 09:55:16 +0700

    euler

--- FirstProject/projectEuler.java
+++ FirstProject/projectEuler.java
@@ -7,13 +7,56 @@
  */
 public class projectEuler
 {
-    public static int problem1 (int topNumber) {
+    public static int problem1 () {
         int sum = 0;
-        for (int i = topNumber; i > 0; i--) {
-            if (i%3 == 0 || i%5 == 0) {
+        for (int i = 1; i < 1000; i++) {
+            if (i % 3 == 0 || i % 5 == 0) {
                 sum += i;
             }
         }
         return sum;
     }
+    
+    public static int problem2 () {
+        int bigNum = 1;
+        int oldNum = 1;
+        int returnv = 0;
+        int temp = 0;
+        while (bigNum < 4000000) {
+            if ((bigNum + oldNum)%2 == 0) {
+                returnv += (bigNum+oldNum);
+            }
+            temp = bigNum;
+            bigNum = bigNum + oldNum;
+            oldNum = temp;
+        }
+        return returnv;
+    }
+    
+    public static int problem3 () {
+    
+        boolean isPrime = true;
+        
+        int largestPrime = 0;
+        
+        for (int i = 1; i < 600; i++) { //600851475143
+            
+            isPrime = true;
+            for (int j = i; j > 0; j--) {
+            
+                if (i%j == 0) {
+                    isPrime = false;
+                }
+                
+            }
+            
+            if (isPrime == true) {
+                largestPrime = i;
+            }
+        
+        }
+        return largestPrime;
+    
+    }
+    
 }
