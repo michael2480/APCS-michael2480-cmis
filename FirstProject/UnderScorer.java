afb58381c61da72667e1ccfd24070dbd040a8dfd
cmis <cmis@cmis-hs03.(none)>
2017-09-12 11:32:50 +0700

    second commit

--- FirstProject/UnderScorer.java
+++ FirstProject/UnderScorer.java
@@ -1,7 +1,9 @@
 public class UnderScorer
 {
     public static void main(String[] args){
-        
+        for (int i = 1; i != 100; i++) {        
+            System.out.println(underScore(i));
+        }
     }
 
     /**
