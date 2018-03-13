4b7c121086bf8dc174390e492bb8d3abd981c8aa
m <m>
2018-03-13 14:00:23 +0700

    m

---
+++ Abstract/PeopleProgram.java
@@ -0,0 +1,6 @@
+
+public class PeopleProgram
+{
+    Teacher APComSciTeacher = new Teacher();
+    Principal Willette = new Principal();
+}
---
+++ Abstract/Principal.java
@@ -0,0 +1,5 @@
+
+public class Principal extends Person
+{
+    
+}
